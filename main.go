ed906fa131009595120d0658374897d209ff3d3e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2022-09-24 10:37:37 +0900

    単体テストができるようにリファクタリングした

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
@@ -14,13 +17,27 @@ func run(ctx context.Context) error {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
-	s.ListenAndServe()
-	return nil
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	return eg.Wait()
 }
 
 func main() {
 	if err := run(context.Background()); err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
+		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
